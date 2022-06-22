42ffe6501825abb7eef805134dfd4e3d8d60d83a
otcz <carrillozuletaoscar@gmail.com>
2022-06-22 08:56:07 -0500

    FRONT-END TERMINADO 28

--- src/main/java/TaosBackend/Taos/Controller/EnviarSOATGmail.java
+++ src/main/java/TaosBackend/Taos/Controller/EnviarSOATGmail.java
@@ -1,10 +1,6 @@
 package TaosBackend.Taos.Controller;
 
-import javax.mail.*;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
+
 
 public class EnviarSOATGmail {
 
@@ -24,40 +20,7 @@ public class EnviarSOATGmail {
 
     }
 
-    public boolean enviarCorreo() {
 
-        try {
-            Properties props = new Properties();
-            props.setProperty("mail.smtp.host", "smtp.gmail.com");
-            props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.port", "587");
-            props.setProperty("mail.smtp.auth", "true");
-
-
-
-            Session session = Session.getDefaultInstance(props);
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(getsCorreoQueEnvia()));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(getsDestinatario()));
-            message.setSubject(getsAsunto());
-            message.setText(getsCuerpo(), "ISO-8859-1", "html");
-
-            Transport t = session.getTransport("smtp");
-            t.connect(getsCorreoQueEnvia(), getsClaveCorreoQueEnvia());
-            t.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
-            t.close();
-
-
-        } catch (AddressException ex) {
-
-        } catch (MessagingException ex) {
-            ex.printStackTrace();
-        }
-
-
-        return true;
-
-    }
 
     public String getsDestinatario() {
         return sDestinatario;
